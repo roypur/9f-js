7c8eaadad4bd7cd4bc2d92722bbb9d3393a466be
Roy Olav Purser <admin@royolav.net>
2015-05-04 18:19:34 +0200

    test

--- 9f.js
+++ 9f.js
@@ -45,6 +45,6 @@ f.api = function (data, url, handle)
     var req = new XMLHttpRequest();
     req.open('POST', url, true);
     req.setRequestHeader('x-api', data);
-    req.onreadystatechange = function(a){console.log(a)}
+    req.onreadystatechange = function(a){handle(a)};
     req.send();
 }
